ed538bd4974f9d4ff69ca5c7864b8ebcfb09fb8c
Kendy <kndysong@gmail.com>
2022-08-13 12:30:18 +0200

    set timer methods to noexcept

--- src/application/Timer.cpp
+++ src/application/Timer.cpp
@@ -5,24 +5,24 @@ Timer::Timer()
 	Start();
 }
 
-void Timer::Start()
+void Timer::Start() noexcept
 {
 	_start = std::chrono::system_clock::now();
 	_isRunning = true;
 }
 
-void Timer::Restart()
+void Timer::Restart() noexcept
 {
 	Start();
 }
 
-void Timer::Stop()
+void Timer::Stop() noexcept
 {
 	_stop = std::chrono::system_clock::now();
 	_isRunning = false;
 }
 
-double Timer::GetElapsedTime()
+double Timer::GetElapsedTime() noexcept
 {
 	if (_isRunning)
 		_elapsed = std::chrono::system_clock::now();
